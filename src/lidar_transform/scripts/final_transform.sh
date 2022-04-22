b8d161db030e42687fcb7ba033f70675c58ce823
vishrutskaushik <vishrutskaushik@gmail.com>
2022-04-22 17:55:42 -0400

    Working Velodyne Point Cloud + Transform for VLP-16

---
+++ src/lidar_transform/scripts/final_transform.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+CALIBRATION_DIR=$2
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$DIR/.."
+
+EXTRINSICS_FILES=(
+  "${CALIBRATION_DIR}/final_extrinsic.yaml"
+)
+
+function help() {
+  echo 'Options:
+    publish: broadcast the extrinsic
+    clean: extrinsics clean
+  '
+}
+
+function clean() {
+  ps -ef | grep static_transform_publisher | awk '{print $2}' | xargs kill -2
+}
+
+function publish() {
+  for i in "${EXTRINSICS_FILES[@]}"
+  do
+    python ${DIR}/extrinsics_broadcaster.py $i &
+  done
+}
+
+case $1 in
+  publish)
+    # Clean the previous Transforms
+    clean $@
+
+    #Publish new transforms
+    publish $@
+    ;;
+  clean)
+    clean $@
+    ;;
+  *)
+    # Clean the previous Transforms
+    clean $@
+
+    #Publish new transforms
+    publish $@
+    ;;
+esac
